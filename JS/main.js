77de4a26d46e567e03cf0ac99804416aa1cc40f1
Diego Eme Rodriguez <rdz.diegom@gmail.com>
2022-08-28 17:51:59 -0300

    Ajustes js.

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,41 @@
-document.getElementById("btnIniciarSesion").addEventListener("click",iniciarSecion);
-document.getElementById("btnRegistrarse").addEventListener("click",registro);
+let btnIniciarSesion = document.getElementById("btnIniciarSesion")
+btnIniciarSesion.onclick = () => {
+    if(window.innerWidth>850){
+        formularioRegistro.style.display = "none";
+        contenedorLoginRegistro.style.left = "10px";
+        formularioLogin.style.display = "block";
+        cajaRegistro.style.opacity = "1";
+        cajaLogin.style.opacity = "0";
+    }
+    else{
+        formularioRegistro.style.display = "none";
+        contenedorLoginRegistro.style.left = "0px";
+        formularioLogin.style.display = "block";
+        cajaRegistro.style.display = "block";
+        cajaLogin.style.display = "none";
+    }    
+}
+
+let btnRegistrarse = document.getElementById("btnRegistrarse")
+btnRegistrarse.onclick = () =>{
+    if(window.innerWidth>850){
+        formularioRegistro.style.display = "block";
+        contenedorLoginRegistro.style.left = "410px";
+        formularioLogin.style.display = "none";
+        cajaRegistro.style.opacity = "0";
+        cajaLogin.style.opacity = "1";
+    }
+    else{
+        formularioRegistro.style.display = "block";
+        contenedorLoginRegistro.style.left = "0px";
+        formularioLogin.style.display = "none";
+        cajaRegistro.style.display = "none";
+        cajaLogin.style.display = "block";
+        cajaLogin.style.opacity = "1";
+    }
+}
 
 window.addEventListener("resize", ajusteWidth);
-
-
-// Declaración de variables
-let contenedorLoginRegistro = document.querySelector(".contenedorLoginRegistro");
-let formularioLogin = document.querySelector(".formularioLogin");
-let formularioRegistro = document.querySelector(".formularioRegistro");
-let cajaLogin = document.querySelector(".cajaLogin");
-let cajaRegistro = document.querySelector(".cajaRegistro");
 
 function ajusteWidth() {
     if(window.innerWidth > 850){
@@ -27,41 +53,3 @@ function ajusteWidth() {
 }
 
 ajusteWidth();
-
-function iniciarSecion(){
-    if(window.innerWidth>850){
-        formularioRegistro.style.display = "none";
-        contenedorLoginRegistro.style.left = "10px";
-        formularioLogin.style.display = "block";
-        cajaRegistro.style.opacity = "1";
-        cajaLogin.style.opacity = "0";
-    }
-    else{
-        formularioRegistro.style.display = "none";
-        contenedorLoginRegistro.style.left = "0px";
-        formularioLogin.style.display = "block";
-        cajaRegistro.style.display = "block";
-        cajaLogin.style.display = "none";
-    }
-
-}
-
-function registro(){
-    if(window.innerWidth>850){
-        formularioRegistro.style.display = "block";
-        contenedorLoginRegistro.style.left = "410px";
-        formularioLogin.style.display = "none";
-        cajaRegistro.style.opacity = "0";
-        cajaLogin.style.opacity = "1";
-    }
-    else{
-        formularioRegistro.style.display = "block";
-        contenedorLoginRegistro.style.left = "0px";
-        formularioLogin.style.display = "none";
-        cajaRegistro.style.display = "none";
-        cajaLogin.style.display = "block";
-        cajaLogin.style.opacity = "1";
-    }
-
-}
-
---
+++ JS/variables.js
@@ -0,0 +1,6 @@
+// Declaración de variables
+let contenedorLoginRegistro = document.querySelector(".contenedorLoginRegistro");
+let formularioLogin = document.querySelector(".formularioLogin");
+let formularioRegistro = document.querySelector(".formularioRegistro");
+let cajaLogin = document.querySelector(".cajaLogin");
+let cajaRegistro = document.querySelector(".cajaRegistro");
